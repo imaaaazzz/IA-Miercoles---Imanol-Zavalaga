b3cf6d4b12cd7623439164c06acde4865d0c5efd
imaaaazzz <164021589+imaaaazzz@users.noreply.github.com>
2025-11-05 20:11:23 -0300

    Update MiChat.py

--- MiChat.py
+++ MiChat.py
@@ -8,7 +8,7 @@ nombre = st.text_input("Cual es tu nombre?")
 if st.button("Saludar!"):
     st.write(f"Hola {nombre}! Bienvenido a talento tech")
 
-MODELOS = ['llama-3.1-8b-instant', 'llama-3.3-70b-versatile', 'deepseek-r1-distill-llama-70b']
+MODELOS = ['llama-3.1-8b-instant', 'llama-3.3-70b-versatile']
 
 def configurar_pagina():
     st.title("Talk to the AI ")
